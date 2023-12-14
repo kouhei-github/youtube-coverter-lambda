446cdf8e90786959c28de9334bffe4323843cd08
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-12-14 17:13:30 +0900

    ダウンローダー機能に期間とファイルサイズを追加
    ダウンロード機能に、duration と filesize という 2 つの新しいプロパティが追加されました。これらのフィールドは、動画情報辞書から抽出され、返される JSON データ オブジェクトに含まれます。これにより、ユーザーはダウンロードに関するより詳細な情報にアクセスできるようになります。

--- src/download.py
+++ src/download.py
@@ -1,15 +1,36 @@
 from yt_dlp import YoutubeDL
+from typing import List
 import uuid
 
-class YoutubeDowonloader:
+class YoutubeDownloader:
+    """
+    YoutubeDownloader
+
+        This class provides a way to download videos from YouTube using the YoutubeDL library. It initializes the object with default values for the options of YoutubeDL.
+
+    Methods:
+
+        __init__(self)
+            Initializes the object with default values for ydl_opts.
+
+        download(self, url_list: List[str])
+            Downloads the videos from the specified URLs and returns information about the downloaded video.
+
+            Args:
+                url_list (List[str]): A list of URLs that need to be downloaded.
+
+            Returns:
+                dict: A dictionary containing information about the downloaded video. The dictionary has the following keys:
+                    - "title": The title of the video, or None if the title is not available.
+                    - "thumbnail": The URL of the video thumbnail, or None if the thumbnail is not available.
+                    - "duration": The duration of the video in seconds, or None if the duration is not available.
+                    - "filesize": The size of the video file in bytes, or None if the filesize is not available.
+    """
+    output_path = f'/tmp/{uuid.uuid4()}'
     def __init__(self) -> None:
-        """Initialize the YoutubeDowonloader
-
-        Args:
-            url_list (list): list of urls
         """
-
-        self.output_path = f'/tmp/{uuid.uuid4()}'
+        Initializes the object with default values for ydl_opts.
+        """
         self.ydl_opts = {
             "format": "mp3/bestaudio/best",
             "postprocessors": [
@@ -20,23 +41,25 @@ class YoutubeDowonloader:
             ],
             "outtmpl": self.output_path,
         }
-        self.thumbnail = ""
-        self.title = ""
-        self.duration = ""
-        self.filesize = ""
 
-
-
-    def download(self, url_list: list):
-        """Download the given urls
-
+    def download(self, url_list: List[str]):
+        """
         Args:
-            urls (list): list of urls
+            url_list (List[str]): A list of URLs that need to be downloaded.
+
+        Returns:
+            dict: A dictionary containing information about the downloaded video. The dictionary has the following keys:
+                - "title": The title of the video, or None if the title is not available.
+                - "thumbnail": The URL of the video thumbnail, or None if the thumbnail is not available.
+                - "duration": The duration of the video in seconds, or None if the duration is not available.
+                - "filesize": The size of the video file in bytes, or None if the filesize is not available.
         """
         with YoutubeDL(self.ydl_opts) as ydl:
             _ = ydl.download(url_list)
             info_dict = ydl.extract_info(url_list[0], download=False)
-            self.title = info_dict.get('title', None)
-            self.thumbnail = info_dict.get('thumbnail', None)
-            self.duration = info_dict.get('duration', None)
-            self.filesize = info_dict.get('filesize', None)
+            return {
+                "title": info_dict.get('title', None),
+                "thumbnail": info_dict.get('thumbnail', None),
+                "duration": info_dict.get('duration', None),
+                "filesize": info_dict.get('filesize', None)
+            }
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import json
 import os
-
-from download import YoutubeDowonloader
+import glob
+from download import YoutubeDownloader
 import boto3
 from botocore.exceptions import NoCredentialsError
 
@@ -9,37 +9,63 @@ s3_client = boto3.client('s3')
 
 bucket_name = 'youtube-mp3-image'
 
+
 def lambda_handler(event, context):
+    """
+    AWS Lambda関数のハンドラー。HTTPリクエストに応答する。
+
+    Args:
+        event (dict): Lambda関数に渡されるイベントデータ。
+        context (object): 実行環境に関する情報を提供するコンテキストオブジェクト。
+
+    Returns:
+        dict: HTTPステータスコード、ヘッダー、ボディを含むレスポンスオブジェクト。
+    """
+
+    # ローカル実行かどうかを判断する
     if __name__ == "__main__":
         body = {"url": "https://www.youtube.com/watch?v=ps-pHi81S_Q"}
     else:
+        # LambdaイベントからボディをJSONとして読み込む
         body = json.loads(event["body"])
-    # pdf_file: str, google_drive_id: str, service_account_file: str
+
+    # 一時ファイルを削除するための関数を呼び出す
+    _delete_tmp_file()
+
+    # 処理するURLリスト
     URL_LIST = [
         body["url"],
-        # Add more URLs as needed
+        # 必要に応じてさらにURLを追加
     ]
 
-    yd = YoutubeDowonloader()
-    yd.download(URL_LIST)
+    # YouTubeダウンローダーのインスタンスを作成
+    youtube_downloader = YoutubeDownloader()
+    # ファイルパスを設定
+    file_path = f'{youtube_downloader.output_path}.mp3'
 
-    file_path = f'{yd.output_path}.mp3'
+    # YouTubeダウンローダーを使用して動画情報を取得
+    movie_info = youtube_downloader.download(URL_LIST)
 
+    # S3にファイルをアップロードし、アップロードされたファイル名を取得
     uploaded_file_name = upload_file_to_s3(file_path, bucket_name, file_path.replace("/tmp/", ""))
+
+    # アップロードに成功した場合
     if uploaded_file_name:
+        # S3オブジェクトのURLを取得
         file_url = get_s3_object_url(bucket_name, uploaded_file_name)
+        # 応答用のJSONデータを作成
         json_data = json.dumps({
             "file_name": file_url,
-            "thumbnail": yd.thumbnail,
-            "title": yd.title,
-            "duration": yd.duration,
-            "filesize": yd.filesize
+            "thumbnail": movie_info["thumbnail"],
+            "title": movie_info["title"],
+            "duration": movie_info["duration"],
+            "filesize": movie_info["filesize"]
         })
-        os.remove(file_path)
+        # HTTPレスポンスを返す
         return {
             'statusCode': 200,
             "headers": {
-                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "POST"
             },
@@ -47,11 +73,48 @@ def lambda_handler(event, context):
         }
 
 
-def get_s3_object_url(bucket, object_name):
+def _delete_tmp_file():
+    """
+
+    Deletes all temporary mp3 files in the specified temporary directory.
+
+    """
+    tmp_dir = "/tmp"
+
+    # get list of mp3 files
+    mp3_files = glob.glob(os.path.join(tmp_dir, "*.mp3"))
+    for mp3_file in mp3_files:
+        os.remove(mp3_file)
+
+def get_s3_object_url(bucket: str, object_name: str):
+    """
+    Args:
+        bucket (str): The name of the S3 bucket where the object is stored.
+        object_name (str): The name of the object for which the URL is generated.
+
+    Returns:
+        str: The URL of the S3 object.
+
+    """
     return f"https://{bucket}.s3.amazonaws.com/{object_name}"
 
 
-def upload_file_to_s3(file_name, bucket, object_name=None):
+def upload_file_to_s3(file_name: str, bucket: str, object_name=None):
+    """
+    Uploads a file to an S3 bucket.
+
+    Args:
+        file_name (str): The name of the file to upload.
+        bucket (str): The name of the S3 bucket to upload the file to.
+        object_name (str, optional): The name to give the object in S3. If not provided, the file name will be used.
+
+    Returns:
+        str: The name of the object in S3 that was created or updated.
+
+    Raises:
+        NoCredentialsError: If there are no credentials available to access the S3 bucket.
+
+    """
     # ファイル名がS3のオブジェクト名として使用されない場合は、オブジェクト名を指定
     if object_name is None:
         object_name = file_name
@@ -61,6 +124,7 @@ def upload_file_to_s3(file_name, bucket, object_name=None):
     except NoCredentialsError:
         return "Credentials not available"
     return object_name
+
 
 
 if __name__ == "__main__":
